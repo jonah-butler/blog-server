96e23373273cb9c5ee3430513cad749812e223ae
Jonah Butler <jonahbutler6@gmail.com>
2025-03-29 08:46:27 -0400

    logging user agent and new ip headers that will be set by nginx

--- middlewares/logging/logging.go
+++ middlewares/logging/logging.go
@@ -3,6 +3,7 @@ package loggingmiddleware
 import (
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -10,10 +11,22 @@ func LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		log.Printf("[%s] %s %s from %s", r.Method, r.URL.Path, r.Proto, r.RemoteAddr)
+		log.Printf("[%s]: %s %s from %s by %s", r.Method, r.URL.Path, r.Proto, getClientIP(r), r.UserAgent())
 
 		next.ServeHTTP(w, r)
 
 		log.Printf("Completed in %v\n", time.Since(start))
 	})
+}
+
+func getClientIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-Forwarded-For")
+	if forwarded != "" {
+		return strings.Split(forwarded, ",")[0]
+	}
+	realIP := r.Header.Get("X-Real-IP")
+	if realIP != "" {
+		return realIP
+	}
+	return r.RemoteAddr
 }
